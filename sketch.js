bc95fc25ba6c891a7427a99e6eb5e82e8c7579e2
Mathew George <mathewgrg@gmail.com>
2019-10-22 18:51:24 -0400

    Added Colours

--- sketch.js
+++ sketch.js
@@ -8,7 +8,7 @@ function draw()
 {
 
 	//set the fill colour to LemonChiffon
-	fill(255, 250, 224);
+	//fill(255, 250, 224);
 
 	//set a thick stroke weight for the black lines
 	strokeWeight(5);
@@ -16,50 +16,97 @@ function draw()
 	//draw the outer rectangle 
 	rect(100, 50, 700, 800);
 
-	//
-	fill(255, 69, 0);
+	//black
+	fill(0, 0, 0);
 	rect(100, 50, 50, 115);
 
+    //yellow
+    fill(255, 255, 0);
 	rect(150, 50, 80, 165);
-
+    
+    //black
+	fill(0, 0, 0);
 	rect(230, 50, 180, 75);
 
+    //crimson red
+    fill(220, 20, 60);
 	rect(410, 50, 50, 105);
 
+    //dodger blue
+    fill(30, 144, 255);
 	rect(460, 50, 95, 220);
 
+    //green
+    fill(0, 128, 0);
 	rect(555, 50, 150, 45);
 
+    //green
+    fill(0, 128, 0);
 	rect(705, 50, 95, 115);
 
+    //green
+    fill(0, 128, 0);
 	rect(100, 165, 50, 115);
 
+    //crimson red
+    fill(220, 20, 60);
 	rect(100, 280, 250, 115);
 
+    //yellow
+    fill(255, 255, 0);
 	rect(100, 395, 150, 215);
 
+    //black
+	fill(0, 0, 0);
 	rect(100, 750, 120, 100);
 
+    //crimson red
+    fill(220, 20, 60);
 	triangle(100, 606, 170, 750);
 
+    //green
+    fill(0, 128, 0);
 	rect(170, 610, 260, 140);
-
+    
+    //yellow
+    fill(255, 255, 0);
 	rect(230, 155, 230, 125);
 
+    //green
+    fill(0, 128, 0);
 	rect(500, 800, 300, 50);
 
+    //dodger blue
+    fill(30, 144, 255);
 	rect(430, 700, 70, 150);
 
+    //white
+    fill(255, 255, 255);
 	rect(630, 730, 170, 70);
 
+    //dodger blue
+    fill(30, 144, 255);
 	rect(250, 395, 500, 115);
 
+    //crimson red
+    fill(220, 20, 60);
 	rect(750, 165, 50, 565);
 
+    //black
+	fill(0, 0, 0);
 	rect(555, 165, 195, 230);
 
+    //dodger blue
+    fill(30, 144, 255);
 	rect(500, 510, 220, 220);
 
-	rect(430, 510, 70, 100)
+    //crimson red
+    fill(220, 20, 60);
+	rect(430, 510, 70, 100);
+    
+    //white
+    fill(255, 255, 255);
+    rect(460, 280, 95, 115);
+    
 	
 }
