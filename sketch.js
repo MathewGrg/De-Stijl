6c5889136f77c12b41c38ab46deb25a0d9ad3ff2
Mathew George <mathewgrg@gmail.com>
2019-10-22 15:22:45 -0400

    Completed Outline

--- sketch.js
+++ sketch.js
@@ -1,19 +1,65 @@
 function setup()
 {
 	//create a large square canvas
-	createCanvas(800, 800);
+	createCanvas(900, 900);
 }
 
 function draw()
 {
 
-	//set the fill colour to red
-	fill(255, 0, 0);
+	//set the fill colour to LemonChiffon
+	fill(255, 250, 224);
 
 	//set a thick stroke weight for the black lines
-	strokeWeight(12);
+	strokeWeight(5);
 
-	//draw the red rectangle 
-	rect(100, 50, 600, 600);
+	//draw the outer rectangle 
+	rect(100, 50, 700, 800);
 
+	//
+	fill(255, 69, 0);
+	rect(100, 50, 50, 115);
+
+	rect(150, 50, 80, 165);
+
+	rect(230, 50, 180, 75);
+
+	rect(410, 50, 50, 105);
+
+	rect(460, 50, 95, 220);
+
+	rect(555, 50, 150, 45);
+
+	rect(705, 50, 95, 115);
+
+	rect(100, 165, 50, 115);
+
+	rect(100, 280, 250, 115);
+
+	rect(100, 395, 150, 215);
+
+	rect(100, 750, 120, 100);
+
+	triangle(100, 606, 170, 750);
+
+	rect(170, 610, 260, 140);
+
+	rect(230, 155, 230, 125);
+
+	rect(500, 800, 300, 50);
+
+	rect(430, 700, 70, 150);
+
+	rect(630, 730, 170, 70);
+
+	rect(250, 395, 500, 115);
+
+	rect(750, 165, 50, 565);
+
+	rect(555, 165, 195, 230);
+
+	rect(500, 510, 220, 220);
+
+	rect(430, 510, 70, 100)
+	
 }
